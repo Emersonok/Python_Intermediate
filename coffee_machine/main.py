35db24d2e656c5fc6267a5be8d2557d1fedcf6c1
Emerson Okorie <emersonokorie@icloud.com>
2023-01-24 08:45:00 -0300

    update

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -29,11 +29,12 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-quarter = 0
-dime = 0
-nickle = 0
-penny = 0
+espresso_cost = 1.5
+latte_cost = 2.5
+cappuccino_cost = 3.0
+change = 0
 stop = False
+
 def check_resources():
     if resources["water"] < 0:
         return "Sorry, there isn't enough water"
@@ -42,6 +43,22 @@ def check_resources():
     if resources["milk"] < 0:
         return "Sorry there isn't enough milk"
 
+def calculate_change(quarter, penny, dime, nickle):
+    quarter = 0.25 * quarter
+    penny = 0.01 * penny
+    dime = 0.10 * dime
+    nickle = 0.05 * nickle
+    money = quarter + penny + dime + nickle
+    if order == "espresso":
+        change = money - espresso_cost
+        return f"Your change is {change}"
+    elif order == "latte":
+        change = money - latte_cost
+        return f"Your change is {change}"
+    elif order == "cappuccino":
+        change = money - cappuccino_cost
+        return f"Your change is {change}"
+
 #getting the clients'orders
 while not stop:
   order = input("What would you like? (espresso/latte/cappuccino): ")
@@ -49,32 +66,30 @@ while not stop:
     print(resources)
     
   elif order == "espresso":
-    check_resources()
     resources["water"] -= 50
     resources["coffee"] -= 18
-    print(check_resources())
     
   elif order == "latte":
-    check_resources()
     resources["water"] -= 200
     resources["coffee"] -= 24
     resources["milk"] -= 150
-    print(check_resources())
     
   elif order == "cappuccino":
-    check_resources()
     resources["water"] -= 250
     resources["coffee"] -= 24
     resources["milk"] -= 100
+    
+    
+  if resources ["water"] <= 0 or resources["coffee"] <= 0 or resources["milk"] <= 0:
     print(check_resources())
-    
-    
-    #print("Please insert coins ")
+  else:
+    print("Please insert coins ")
 
-    #quarter = int(input("How many quarters?: "))
-    #penny = int(input("How many pennies?: "))
-    #dime = int(input("How many dimes?: "))
-    #nickle = int(input("How many nickles?: "))
+    quarter = float(input("How many quarters?: "))
+    penny = float(input("How many pennies?: "))
+    dime = float(input("How many dimes?: "))
+    nickle = float(input("How many nickles?: "))
+    print(calculate_change(quarter, penny, dime, nickle))
 
   if input("Turn off the machine? off or no ") == "off":
     stop = True
